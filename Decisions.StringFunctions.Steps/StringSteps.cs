c01b15ade69610edb4c2c44e7729f3a682d1699f
Kenny Vigersand <kenny.vigersand@zitac.se>
2022-11-10 10:36:53 +0100

    Initial commit

---
+++ Decisions.StringFunctions.Steps/StringSteps.cs
@@ -0,0 +1,23 @@
+﻿using DecisionsFramework.Design.Flow;
+using System.Globalization;
+using System.Text;
+
+namespace Decisions.StringFunctions.Steps;
+
+[AutoRegisterMethodsOnClass(true, "Data", "Text")]
+public class StringSteps
+{
+    public static string RemoveAccents(string text){     
+    StringBuilder sbReturn = new StringBuilder();     
+    var arrayText = text.Normalize(System.Text.NormalizationForm.FormD).ToCharArray();  
+   foreach (char letter in arrayText){     
+        if (CharUnicodeInfo.GetUnicodeCategory(letter) != UnicodeCategory.NonSpacingMark)  
+        sbReturn.Append(letter);
+    }
+    byte[] tempBytes = Encoding.GetEncoding("ISO-8859-8").GetBytes(sbReturn.ToString());
+    string asciiStr = Encoding.UTF8.GetString(tempBytes);
+    return asciiStr;
+} 
+
+
+}
