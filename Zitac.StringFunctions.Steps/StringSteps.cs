18dcfe231ec5f6c60a82a1ff8e914549096356c1
Vigersand <kenny.vigersand@zitac.se>
2023-10-20 14:44:59 +0200

    Added Docker string parse, created module

--- Zitac.StringFunctions.Steps/StringSteps.cs
+++ Zitac.StringFunctions.Steps/StringSteps.cs
@@ -18,6 +18,37 @@ public class StringSteps
     string asciiStr = Encoding.UTF8.GetString(tempBytes);
     return asciiStr;
 } 
+public static string DecodeDockerLog(byte[] input)
+{
+    int offset = 0;
+    var output = new List<byte>();
 
+    while (offset < input.Length)
+    {
+        // Check if there's at least 8 bytes left for the header
+        if (offset + 8 > input.Length)
+            throw new InvalidOperationException("Unexpected end of data, header incomplete.");
+
+        // Extract the size of the data frame from the header bytes 4-7 (big-endian)
+        int dataSize = BitConverter.ToInt32(new[] { input[offset + 7], input[offset + 6], input[offset + 5], input[offset + 4] });
+
+        // Check if there's enough data left for the data content
+        if (offset + 8 + dataSize > input.Length)
+            throw new InvalidOperationException("Unexpected end of data, content incomplete.");
+
+        // Copy the content data to the output, skipping the header
+        for (int i = 0; i < dataSize; i++)
+        {
+            output.Add(input[offset + 8 + i]);
+        }
+
+        // Move the offset to the next frame
+        offset += 8 + dataSize;
+    }
+
+    return Encoding.UTF8.GetString(output.ToArray());
+}
 
 }
+
+
